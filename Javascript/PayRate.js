d70faa97e6b430779c66eae57488c72d1e167543
Andrew Jabbour <ajabbour@Sultans-MacBook-Pro.local>
2018-02-15 15:45:18 -0500

    commit

--- Javascript/PayRate.js
+++ Javascript/PayRate.js
@@ -1,8 +1,41 @@
 $(document).ready(function(){
-    $("#amount").val("Hello World!");
+    $("#convert").click(function (){
+        
+        if($("#frequencyList").val() == "Hourly"){
+            alert("Hourly");
+        } 
+        if($("#frequencyList").val() == "Daily"){
+            alert("Daily");
+        }
+        if($("#frequencyList").val() == "Weekly"){
+            alert("Weekly");
+        }
+        if($("#frequencyList").val() == "Monthly"){
+            alert("Monthly");
+        }
+        if($("#frequencyList").val() == "Annual"){
+            alert("Annual");
+        }
+
+    });
 });
 
+function hourlyPay(){
 
-function weeklyPay(daily){
-    return daily * 5;
+}
+
+function dailyPay(){
+
+}
+
+function weeklyPay(){
+    
+}
+
+function monthlyPay(){
+
+}
+
+function annualPay(){
+
 }
